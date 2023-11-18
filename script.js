adccda05d9cc6512d9c5320568bb56afae0888c1
abd074 <badawyabdulrahman099@gmail.com>
2023-11-18 23:47:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let btns = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    this.classList.toggle("active ")
+
+    let desc = this.nextElementSibling;
+    if (desc.style.maxHeight) {
+      desc.style.maxHeight = null;
+    } else {
+      desc.style.maxHeight = desc.scrollHeight + "px";
+    }
+  });
+}
