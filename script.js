15417085f7b8824cb760028e1003cb6afe337eb1
abd074 <badawyabdulrahman099@gmail.com>
2023-11-19 02:04:46 +0200

    correct error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let btns = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    this.classList.toggle("active ")
+    this.classList.toggle("active");
 
     let desc = this.nextElementSibling;
     if (desc.style.maxHeight) {
